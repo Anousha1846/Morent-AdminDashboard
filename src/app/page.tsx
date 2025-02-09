ed02e9e74cd0330c2b86ba2db701bbc4af32991a
Anousha1846 <anoushaasadullah18@gmail.com>
2025-02-09 22:19:42 +0500

    Resolving Page Not Found error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
 <div>
-<main className="flex flex-col gap-9 items-center justify-center min-h-screen w-screen bg-gray-100">
+<main className="flex flex-col gap-9 items-center justify-center min-h-full w-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-700">Admin Pannel</h1>
       <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-800 transition">
-  <Link href="/admin">Go To Admin Dashboard</Link>
+  <Link href="/admin/login">Go To Admin Dashboard</Link>
 </button>
     </main>
   
